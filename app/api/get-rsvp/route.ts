7b95ffbebbcf66ee4a2a5334327ffb2a002a626f
Juan Drada <jdrada.dev@gmail.com>
2024-09-03 19:05:47 -0600

    filter tests :)

--- app/api/get-rsvp/route.ts
+++ app/api/get-rsvp/route.ts
@@ -17,11 +17,11 @@ export async function GET(request: Request) {
 
     if (!rows || rows.length === 0) throw new Error("No data found");
 
-    // const filteredTests = rows.filter(
-    //   (rows) => rows.name.toLowerCase() !== "test"
-    // );
+    const filteredTests = rows.filter(
+      (rows) => rows.name.toLowerCase() !== "test"
+    );
 
-    return NextResponse.json({ data: rows }, { status: 200 });
+    return NextResponse.json({ data: filteredTests }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
